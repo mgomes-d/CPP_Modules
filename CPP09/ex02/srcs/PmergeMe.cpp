58252a1ae99013f54d5a35c584e728b025ad54a6
IronSpiderr <murillo.jogos8@hotmail.com>
2023-07-28 12:34:20 +0200

    CPP09 ex02 start

--- CPP09/ex02/srcs/PmergeMe.cpp
+++ CPP09/ex02/srcs/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: mgomes-d <mgomes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 11:00:34 by mgomes-d          #+#    #+#             */
-/*   Updated: 2023/07/27 08:06:15 by mgomes-d         ###   ########.fr       */
+/*   Updated: 2023/07/28 11:53:21 by mgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,9 +58,53 @@ bool PmergeMe::_compareIntegers(int a, int b)
 
 void PmergeMe::_fordJohnsonListSort(void) 
 {
-   // std::list<int> copyList = *this->_unsortedList;
+	std::list<int> copyList = *this->_unsortedList;
+	std::list<int> sorted;
+	std::list<int> PairA;
+	std::list<int> PairB;
+	std::size_t i = 0;
+	//Pair Seperation
+	for (std::list<int>::iterator it = copyList.begin(); it != copyList.end(); ++it)
+	{
+		if (i % 2 == 0)
+			PairB.push_back(*it);
+		else
+			PairA.push_back(*it);
+		i++;
+	}
+	// Comparation A < B
+	std::list<int>::iterator itB = PairB.begin();
+	for (std::list<int>::iterator itA = PairA.begin(); itA != PairA.end(); ++itA)
+	{
+		if (this->_compareIntegers(*itA, *itB)){
+			int temp;
+			temp = *itA;
+			*itA = *itB;
+			*itB = temp;
+		}
+		++itB;
+	}
+	// insering b3 among a1 et a2
+	for (std::list<int>::iterator itA = PairA.begin(); itA != PairA.end(); ++itA)
+	{
+		//comparing a1 avec a2
+		if (std::next(itA) != PairA.end()) {
+            if (!this->_compareIntegers(*itA, *std::next(itA))) {
+				std::iter_swap(itA, std::next(itA));
+            }
+			//put e inside
+			
+		}
+	}
 
-    std::cout << std::endl;
+	for (std::list<int>::iterator it = PairA.begin(); it != PairA.end(); it++) {
+        std::cout << *it << " ";
+    }
+	std::cout << std::endl;
+	for (std::list<int>::iterator it = PairB.begin(); it != PairB.end(); it++) {
+        std::cout << *it << " ";
+    }
+	std::cout << std::endl;
 }
 
 void PmergeMe::sort(void)
--- CPP09/ex02/srcs/main.cpp
+++ CPP09/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgomes-d <mgomes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 11:11:33 by mgomes-d          #+#    #+#             */
-/*   Updated: 2023/07/26 10:53:51 by mgomes-d         ###   ########.fr       */
+/*   Updated: 2023/07/28 09:43:50 by mgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ int	main(int ac, char **av)
 	(void)av;
 	int i = 0;
 	std::srand(static_cast<unsigned>(std::time(0)));
-	int	*array = new int[20];
-	while (i < 20)
+	int	*array = new int[21];
+	while (i < 21)
 	{
 		array[i] = std::rand() % 20;
 		i++;
 	}
-	PmergeMe test(array, 20);
+	PmergeMe test(array, 21);
 	test.sort();
 	delete [] array;
 }
